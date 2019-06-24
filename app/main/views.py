59675e04d20eff21e1909b2e19f3a7772b623395
gkarumba <gachegua@gmail.com>
2019-06-24 09:38:48 +0300

    Add the get articles by category views

--- app/main/views.py
+++ app/main/views.py
@@ -13,6 +13,32 @@ def index():
     message = 'Home - Welcome to The best Online News Source'
     search_news = request.args.get('news_name')
     if search_news:
-        return redirect(url_for('search',news_name = search_news))
+        return redirect(url_for('search.html',news_name = search_news))
     else:
         return render_template('index.html',message = message, title = title)
+
+@main.route('/sports')
+def sport():
+	'''
+	View root page function that returns the sports page and its data
+	'''
+
+	sports = get_news_article('sports')
+	return render_template('sports.html',news_articles=sports)
+
+@main.route('/business')
+def business():
+	'''
+	View root page function that returns the business page and its data
+	'''
+
+	business = get_news_article('business')
+	return render_template('business.html',news_articles=business)
+
+@main.route('/entertainment')
+def entertainment():
+	'''
+	View root page function that returns the entertainment page and its data
+	'''s
+	entertainment = get_news_article('entertainment')
+	return render_template('entertainment.html',title = title,news_articles=entertainment)
