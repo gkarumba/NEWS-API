3ed81bd28fd63b88b530cd23e3f3c5f7b18b37d5
gkarumba <gachegua@gmail.com>
2019-06-24 05:07:29 +0300

    Add the requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     
--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL =''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&language=en&apiKey={}'
+    NEWS_ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
